06cb37bc142bb0b50660d4d5e60e323cf029825e
lex hui <lexhui1999@gmail.com>
2021-04-13 15:54:23 +0800

    include until

--- TwitterScraping/bash.sh
+++ TwitterScraping/bash.sh
@@ -3,7 +3,7 @@ mkdir -p tweet_jsons
 input="follows.txt"
 while IFS= read -r line
 do
-  test="snscrape --jsonl --since 2020-09-01 twitter-user ${line} > ./tweet_jsons/${line}.json"
+  test="snscrape --jsonl --since 2020-09-01 twitter-search 'from:${line} until:2021-03-31' > ./tweet_jsons/${line}.json"
   output=$(eval "$test")
   echo "$output"
 done < "$input"
